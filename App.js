17922e9cabd7bb7ff3404dc6e5740fb424def044
T-nobu <141105736+T-nobu@users.noreply.github.com>
2026-05-04 00:28:10 +0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,43 +1,6 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import HomeScreen from './screens/HomeScreen';
-import ContactsScreen from './screens/ContactsScreen';
-import ContactDetailsScreen from './screens/ContactDetailsScreen';
-import ScheduleScreen from './screens/ScheduleScreen';
-import MapScreen from './screens/MapScreen';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function TabNavigator() {
-  return (
-    <Tab.Navigator screenOptions={{ headerStyle: { backgroundColor: '#4CAF50' }, headerTintColor: '#fff' }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Contacts" component={ContactsScreen} />
-      <Tab.Screen name="Schedule" component={ScheduleScreen} />
-      <Tab.Screen name="Map" component={MapScreen} />
-    </Tab.Navigator>
-  );
-}
+import React from "react";
+import AppNavigator from "./navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Main" 
-          component={TabNavigator} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="Details" 
-          component={ContactDetailsScreen}
-          options={{ title: 'Contact Details' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+  return <AppNavigator />;
+}  
